a39ac8bc99b4723816522d284d3305c0dcc520a9
Nipitpon Chantada <nipitpon.cha@ascendcorp.com>
2019-04-10 13:54:29 +0700

    edit heroku file

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-var port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+var port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send(`Hello World! from ${port}`))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
